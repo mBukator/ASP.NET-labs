369a94bcb01a5946aba8d488ef10c4eb4d034a73
mBukator <m.bukator03@gmail.com>
2023-09-05 10:56:11 +0300

    first commit

---
+++ LR1/LR1/CompanyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR1 {
+    [Route("/company")]
+    [ApiController]
+    public class CompanyController : ControllerBase {
+
+        [HttpGet]
+        public ActionResult<string> GetCompany() {
+            var company = new Company();
+
+            company.companyCeo = "Max Bukator";
+            company.companyName = "Apple Inc.";
+            company.companyPhone = "+380777777777";
+
+            string companyCeo = company.companyCeo;
+            string companyName = company.companyName;
+            string companyPhone = company.companyPhone;
+
+            return "Company Name: " + companyName + " Company CEO: " + companyCeo + " Company Phone: " + companyPhone;
+
+        } 
+    }
+}
---
+++ LR1/LR1/RandomController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR1 {
+    [Route("/random")]
+    [ApiController]
+    public class RandomController : ControllerBase {
+
+        [HttpGet]
+        public ActionResult<int> GetRandom() { 
+            var random = new Random();
+            var value = random.Next(1, 100);
+
+            return value;
+        }
+    }
+}
